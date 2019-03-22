692849a70452364bdb99b946fceec09652f1f433
dosisod <39638017+dosisod@users.noreply.github.com>
2019-03-21 17:10:10 -0700

    added type flexibility

--- nu.js
+++ nu.js
@@ -1,12 +1,15 @@
 nu=(name, attribs, append)=>{
-	var node=document.createElement(name)
+	var node=document.createElement(name) //make the element we will be modifying
 
 	for (var attrib in attribs) {
-		node[attrib]=attribs[attrib]
+		node[attrib]=attribs[attrib] //set property of node to each passed prop
 	}
 
-	if (append) {
+	if (typeof append=="object") { //if an html object append directly
 		append.appendChild(node)
+	}
+	else if (typeof append=="string") { //if string assume its an id, grab that id and append
+		document.getElementById(append).appendChild(node)
 	}
 	else {
 		return node
