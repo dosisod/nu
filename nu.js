5f46ded712a982c082d3777d8e8a15f1d8a22572
dosisod <39638017+dosisod@users.noreply.github.com>
2019-03-27 23:25:32 -0700

    added append chaining

--- nu.js
+++ nu.js
@@ -5,11 +5,25 @@ nu=(name, attribs, append)=>{
 		node[attrib]=attribs[attrib] //set property of node to each passed prop
 	}
 
-	if (typeof append=="object") { //if an html object append directly
-		append.appendChild(node)
+	var f=e=>{ //allows for append chaining
+		var type=e.constructor.name //name of variable type
+		
+		if (type.substring(0, 4)=="HTML") { //if an html object append directly
+			node=e.appendChild(node).parentNode
+		}
+		else if (type=="String") { //if string assume its an id, grab that id and append
+			node=document.getElementById(e).appendChild(node).parentNode
+		}
 	}
-	else if (typeof append=="string") { //if string assume its an id, grab that id and append
-		document.getElementById(append).appendChild(node)
+
+	if (append) {
+		var list=[]
+		if (!Array.isArray(append)) list.push(append) //append to list if not a list
+		else list=append //set to list if already a list
+		
+		list.forEach(e=>{ //append element into each other element
+			f(e)
+		})
 	}
 	else {
 		return node
