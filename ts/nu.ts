502d90dac72a6b59c2818f66ccf9c4db327c28b3
dosisod <39638017+dosisod@users.noreply.github.com>
2020-01-13 23:04:29 -0800

    cleaned and optimized code

--- ts/nu.ts
+++ ts/nu.ts
@@ -1,46 +1,44 @@
 /*! MIT Licnse for nu available at -> https://github.com/dosisod/nu */
 
-//Complex is a "smart" way to pass/get HTML objects:
-//it can handle HTMLElements, other nu elements, and strings
-//if a string is passed, it is assumed to be an id, so document.getElementById is called
-//Complex can also be an array of complex elements
+type Appendable=(
+	HTMLElement[] |
+	HTMLElement |
+	string[] |
+	string
+)
 
-type _Complex=( HTMLElement | string ) //_Complex is the either HTMLElement or string
-type Complex=( _Complex[] | _Complex ) //Complex can be an array or a single _Complex
-
-function nu(name: string, attribs?: {[attrib: string]: any}, append?: Complex, keep?: boolean): HTMLElement | null {
-	if (!attribs) { //by using nu() without params, you will just get an elemnt by id
+function nu(name: string, attribs?: {[attrib: string]: any}, appendTo?: Appendable, returnOld?: boolean): HTMLElement | null {
+	if (!attribs) {
 		return document.getElementById(name)
 	}
-	let node=document.createElement(name) //make the element we will be modifying
+	let currentNode=document.createElement(name)
 
-	if (attribs) {
-		for (const attrib in attribs) {
-			node[attrib]=attribs[attrib] //set property of node to each passed prop
-		}
+	for (const attrib in attribs) {
+		currentNode[attrib]=attribs[attrib]
 	}
-	let old=node
+	const oldNode=currentNode
 
-	function iter(data: HTMLElement | string): void { //allows for append chaining
-		if (data instanceof HTMLElement) { //if an html object append directly	
-			node=<HTMLElement>data.appendChild(node).parentNode
+	function append(data: HTMLElement | string): void {
+		if (data instanceof HTMLElement) {
+			currentNode=<HTMLElement>data.appendChild(currentNode).parentNode
 		}
-		else if (typeof data==="string") { //if string assume its an id, grab that id and append
-			node=<HTMLElement>document.getElementById(data).appendChild(node).parentNode
+		else if (typeof data==="string") {
+			currentNode=<HTMLElement>document.getElementById(data).appendChild(currentNode).parentNode
 		}
 	}
 
-	if (append) {
-		let list=[]
-		if (!Array.isArray(append)) list.push(append) //append to list if not a list
-		else list=append //set to list if already a list
-		
-		list.forEach(function(e) { //append element into each other element
-			iter(e)
-		})
+	if (appendTo) {
+		if (Array.isArray(appendTo)) {
+			appendTo.forEach(function(e) {
+				append(e)
+			})
+		}
+		else {
+			append(appendTo)
+		}	
 	}
-	if (keep) {
-		return old //returns original element (before appending)
+	if (returnOld) {
+		return oldNode
 	}
-	return node //returns element after being appended
+	return currentNode
 }
