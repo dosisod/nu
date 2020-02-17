c041942869e0aa16801f59276a131cce86a9b12d
dosisod <39638017+dosisod@users.noreply.github.com>
2020-02-16 20:30:05 -0800

    fixed implicit any related TS errors

--- ts/nu.ts
+++ ts/nu.ts
@@ -1,11 +1,8 @@
 /*! MIT Licnse for nu available at -> https://github.com/dosisod/nu */
 
-type Appendable=(
-	HTMLElement[] |
-	HTMLElement |
-	string[] |
-	string
-)
+type AppendableBase=HTMLElement | string
+
+type Appendable=AppendableBase | AppendableBase[]
 
 function nu(name: string, attribs?: {[attrib: string]: any}, appendTo?: Appendable, returnOld?: boolean): HTMLElement | null {
 	if (!attribs) {
@@ -14,7 +11,7 @@ function nu(name: string, attribs?: {[attrib: string]: any}, appendTo?: Appendab
 	let currentNode=document.createElement(name)
 
 	for (const attrib in attribs) {
-		currentNode[attrib]=attribs[attrib]
+		(currentNode as {[name: string]: any})[attrib]=attribs[attrib]
 	}
 	const oldNode=currentNode
 
@@ -29,13 +26,13 @@ function nu(name: string, attribs?: {[attrib: string]: any}, appendTo?: Appendab
 
 	if (appendTo) {
 		if (Array.isArray(appendTo)) {
-			appendTo.forEach(function(e) {
+			appendTo.forEach(function(e: AppendableBase) {
 				append(e)
 			})
 		}
 		else {
 			append(appendTo)
-		}	
+		}
 	}
 	if (returnOld) {
 		return oldNode
